134f6932bd5e3f8794a91f6b087a03d35bd6057a
David John <djohn@arch-no.org>
2020-08-10 13:47:56 -0500

    fixes token headers

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,19 @@
 import ActiveModelAdapter from 'active-model-adapter';
-import TokenAuthorizerMixin from 'ember-simple-auth-token/mixins/token-authorizer';
+import { computed } from '@ember/object';
 import config from '../config/environment';
+import { inject } from '@ember/service';
 
-export default ActiveModelAdapter.extend(TokenAuthorizerMixin, {
-  host: config.host
+export default ActiveModelAdapter.extend({
+
+  session: inject(),
+  host: config.host,
+
+	headers: computed('session.data.authenticated.access', function () {
+    let token = this.get('session.data.authenticated.access')
+    if (token)
+    return {
+			'Authorization': 'Bearer ' + token
+		}
+    return {}
+  }),
 });
