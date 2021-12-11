12382ad8fcd9e81f182cebff340fbcdf0eaa6880
David John <dj@dbjohn.com>
2021-12-11 10:32:55 -0600

    fixes infinity scroll

--- app/controllers/pics/index.js
+++ app/controllers/pics/index.js
@@ -1,19 +1,29 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
 
 export default class PicsIndexController extends Controller {
 
   @service inViewport
   @service session
+  @service store
+  @service inViewport
 
   queryParams = ['tags', 'lat', 'lng']
 
+  @tracked pics = []
+  @tracked tagList = []
+  @tracked isLoaded = false
+
+  @tracked canLoadMore = true
+  @tracked currentPic = false
   @tracked page = 0
-  @computed('tags','lat','lng', 'page')
+
+  @reads('picsTask.isRunning') isLoading
+
   get params(){
     return {
       tags: this.get('tags'),
@@ -24,30 +34,31 @@ export default class PicsIndexController extends Controller {
     }
   }
 
-  @tracked pics = []
-  @tracked tagList = []
+  @(task(function * (params) {
+    const records = yield this.store.query('pic', params)
+    this.set('isLoaded', true)
+    this.get('pics').pushObjects(records.toArray())
+    this.set('tagList', records.get('meta.tag_list'))
+    const totalPages = records.get('meta.total_pages')
+    if (this.get('page') == totalPages)
+      this.set('canLoadMore', false)
+    }).restartable()) picsTask
 
-  canLoadMore = true
-  isLoading = true
-  currentPic = false
-
-  @action
-  tagSelected(tag){
+  @action tagSelected(tag){
     this.set('tags', tag.name)
-    this.clearParams()
+    this.reloadPics()
   }
 
-  @action
-  clearTags(){
+  @action clearTags(){
     this.set('tags', null)
-    this.clearParams()
+    this.reloadPics()
   }
 
-  clearParams(){
+  reloadPics(){
     this.set('page', 0)
     this.set('canLoadMore', true)
-    this.set('isLoading', true)
     this.get('pics').clear()
+    this.set('isLoaded', false)
     this.loadPics()
   }
 
@@ -58,11 +69,28 @@ export default class PicsIndexController extends Controller {
     task.perform(params)
   }
 
-  @action
-  loadNext() {
+  didInsertElement() {
+    set(this, 'viewportSpy', true)
+  }
+
+  @action setupInViewport(){
+    const loader = document.getElementById('load-next')
+    const viewportTolerance = { bottom: 200 }
+    const { onEnter } = this.inViewport.watchElement(loader, { viewportTolerance })
+    onEnter(this.didEnterViewport.bind(this))
+  }
+
+  didEnterViewport() {
+    console.log('loadNext')
     if (this.canLoadMore){
       this.loadPics()
     }
+  }
+
+  willDestroy() {
+    const loader = this.elementId
+    this.inViewport.stopWatching(loader)
+    super.willDestroy(...arguments)
   }
 
 }
--- app/routes/pics/index.js
+++ app/routes/pics/index.js
@@ -1,28 +1,9 @@
 import Route from '@ember/routing/route';
-import { task } from 'ember-concurrency';
 
 export default Route.extend({
 
-  model(){
-    return {
-      pics: this.get('picsTask')
-    }
-  },
-
-  setupController(controller){
-    controller.set('picsTask', this.get('picsTask'))
+  setupController(controller, model) {
     controller.loadPics()
-  },
-
-  picsTask: task(function *(params){
-    const records = yield this.store.query('pic', params)
-    const controller = this.get('controller')
-    controller.get('pics').pushObjects(records.toArray())
-    controller.set('isLoading', false)
-    controller.set('tagList', records.get('meta.tag_list'))
-    const totalPages = records.get('meta.total_pages')
-    if (controller.get('page') == totalPages)
-      controller.set('canLoadMore', false)
-  }).restartable()
+  }
 
 });
